229e3e23f23c7b15c28b27ad960e1accf94aef8b
Jeremy Hubble <1062951+jhubble@users.noreply.github.com>
2024-07-29 13:29:39 -0700

    more info when rebuilding json

--- rerunLibrary.sh
+++ rerunLibrary.sh
@@ -2,3 +2,5 @@
 #ls ~/Downloads/Library.xml|entr ./rerunLibrary.sh
 
 ./cli.js --format json --tracks newTracks.json ~/Downloads/Library.xml ; cat newTracks.json | jq . > newTracksNeat.json ;  mv newTracksNeat.json newTracksConsol.json
+echo "moved"
+date
